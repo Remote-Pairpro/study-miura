930c9fa061d30b2f4b569d9e426966b170dfa5a9
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-11-22 22:58:02 +0900

    課題ロクを追加。

--- js/6_collection_reflect.js
+++ js/6_collection_reflect.js
@@ -1,10 +1,21 @@
-var viewModel = {
-    stringValue : ko.observable("こんにちわ"),
-    passwordValue : ko.observable("hogehoge"),
-    booleanValue : ko.observable(true),
-    optionValues : ["Alpha", "Beta", "Gamma"],
-    selectedOptionValue : ko.observable("Gamma"),
-    multipleSelectedOptionValues : ko.observable(["Alpha"]),
-    radioSelectedOptionValue : ko.observable("Beta")
-};
-ko.applyBindings(viewModel);
+function MyViewModel() {
+    
+    this.firstName = ko.observable('Planet');
+    this.lastName = ko.observable('Earth');
+
+    this.fullName = ko.pureComputed({
+        read: function () {
+            return this.firstName() + " " + this.lastName();
+        },
+        write: function (value) {
+            var lastSpacePos = value.lastIndexOf(" ");
+            if (lastSpacePos > 0) { // Ignore values with no space character
+                this.firstName(value.substring(0, lastSpacePos)); // Update "firstName"
+                this.lastName(value.substring(lastSpacePos + 1)); // Update "lastName"
+            }
+        },
+        owner: this
+    });
+}
+
+ko.applyBindings(new MyViewModel());
