ee6a259d31b6f3f51f4881864a553c405e5cca44
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-01-10 23:34:46 +0900

    typescriptでknockoutjsを使う例を追加。

---
+++ ts/Shampoo.ts
@@ -0,0 +1,16 @@
+/// <reference path="tsd/typings/knockout/knockout.d.ts" />
+class ShampooViewModel {
+    
+    public volume: KnockoutObservable<number> = ko.observable(500);
+
+    public push(): void {
+        if (this.volume() < 100) {
+            this.volume(0);
+            return;
+        }
+        this.volume(this.volume() - 100);
+    }
+}
+
+var viewModel = new ShampooViewModel();
+ko.applyBindings(viewModel);
