9e8f4e12ece47ff2c86f4b38f3b0b667ae52745b
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-01-31 23:13:21 +0900

    とりあえず、２つ目移植実装。

--- ts/2_clickcounter.ts
+++ ts/2_clickcounter.ts
@@ -1,20 +1,43 @@
 /// <reference path="./tsd/typings/tsd.d.ts" />
 /// <reference path="./tsd/typings/knockout/knockout.d.ts" />
 
-
 class ClickCounterViewModel {
+    // プロパティっぽいやつ
+    public numberOfClicks: KnockoutObservable<number>;
+    public hasClickedTooManyTimes: KnockoutComputed<number>;
+    public getMizumashi: KnockoutComputed<number>;
     
+    
+    // コンストラクタ
+    constructor() {
+        // 初期値は作成時の引数から作成。
+        this.numberOfClicks = ko.observable(3);
+        // イベント追加。
+        this.hasClickedTooManyTimes = ko.computed({
+            owner: this,
+            read: function() {
+                return this.numberOfClicks() >= 5;
+            }
+        });
+        this.getMizumashi = ko.computed({
+            owner: this,
+            read: function() {
+                return this.numberOfClicks() * 10;
+            }
+        });
+    }
+
+    public registerClick(): void {
+        this.numberOfClicks(this.numberOfClicks() + 1);
+    }
+    
+    public resetClicks(): void {
+         this.numberOfClicks(0);
+    }
 }
 
 
 // var ClickCounterViewModel = function() {
-//     this.numberOfClicks = ko.observable(3);
-//     this.registerClick = function() {
-//         this.numberOfClicks(this.numberOfClicks() + 1);
-//     };
-//     this.resetClicks = function() {
-//         this.numberOfClicks(0);
-//     };
 //     this.hasClickedTooManyTimes = ko.computed(function() {
 //         return this.numberOfClicks() >= 5;
 //     }, this);
