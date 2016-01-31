84007946cd83d3899e2ecdfe5a426e85dc87df02
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-01-31 22:06:11 +0900

    KnockoutをJSで書いていたものをTypeScriptへと変換。

--- ts/1_first.ts
+++ ts/1_first.ts
@@ -1,24 +1,33 @@
-/// <reference path="tsd/typings/knockout/knockout.d.ts" />
+/// <reference path="./tsd/typings/tsd.d.ts" />
+/// <reference path="./tsd/typings/knockout/knockout.d.ts" />
 
 // ViewModel を定義します
-class ViewModel {
-    constructor(public firstName: string, public lastName: string) {
-        ko.track(this); // プロパティ (name, number) を監視できるようにする
+class MyViewModel {
+    
+    // プロパティっぽいもの
+    public firstName: KnockoutObservable<string>;
+    public lastName: KnockoutObservable<string>;
+    public fullName: KnockoutComputed<string>;
+    
+    // コンストラクタ
+    constructor(firstName: string, lastName: string) {
+        // 初期値は作成時の引数から作成。
+        this.firstName = ko.observable(firstName);
+        this.lastName = ko.observable(lastName);
+        // イベント追加。
+        this.fullName = ko.computed({
+            owner: this,
+            read: function() {
+                // Knockout は依存を自動的にトラッキングします。
+                // fullName の評価中に firstName と lastName を呼び出すため、
+                // それぞれに依存していることが検知されます。
+                return this.firstName() + " " + this.lastName();
+            }
+        });
     }
+    
 }
 
-// TODO ここの素のJSのを上のTypeScript構文に移植する。
-// var ViewModel = function(first, last) {
-//     this.firstName = ko.observable(first);
-//     this.lastName = ko.observable(last);
-//      
-//     this.fullName = ko.computed(function() {
-//         // Knockout は依存を自動的にトラッキングします。
-//         // fullName の評価中に firstName と lastName を呼び出すため、
-//         // それぞれに依存していることが検知されます。
-//         return this.firstName() + " " + this.lastName();
-//     }, this);
-// };
-// 
-// // 次のコードで Knockout を起動します。
-// ko.applyBindings(new ViewModel("Planet", "Earth")); 
+// KnockoutJS側に登録。
+// TODO ここだけは…もうちょっとTSっぽく書ける方法探したいなー。
+ko.applyBindings(new MyViewModel("太郎", "みうみう")); 
