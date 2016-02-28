7cab65ce6b5325f36888da1b41251e06fd726928
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-02-28 19:26:02 +0900

    とりあえず機械的なTS化。一部動かず、型もテキトウ。

--- ts/12_cart.ts
+++ ts/12_cart.ts
@@ -1,3 +1,77 @@
 /// <reference path="./tsd/typings/tsd.d.ts" />
 /// <reference path="./tsd/typings/knockout/knockout.d.ts" />
 /// <reference path="./tsd/typings/jquery.validation/jquery.validation.d.ts" />
+
+// 広域ユティリティ的関数
+function formatCurrency(value:number) {
+    return "$" + value.toFixed(2);
+}
+ 
+class CartLine {
+
+    // プロパティっぽいやつ
+    public category:KnockoutObservable<string>;
+    public product:KnockoutObservable<any>;
+    public quantity:KnockoutObservable<number>;
+    public subtotal:KnockoutComputed<number>;
+
+    // コンストラクタ
+    public constructor() {
+        // Knockout系バインド。        
+        this.category = ko.observable("");
+        this.product = ko.observable(undefined);
+        this.quantity = ko.observable(1);
+        this.subtotal = ko.computed(() => {
+            return this.product() ? this.product().price * parseInt("0" + this.quantity(), 10) : 0;
+        });
+        // カテゴリが変更された時に、製品の選択状態をリセットする
+        this.category.subscribe(() => {
+            this.product(undefined);
+        });
+   }
+    
+}
+ 
+class Cart {
+
+    // プロパティっぽいやつ
+    public lines : KnockoutObservableArray<CartLine>; // デフォルトで1行格納する
+    public grandTotal : KnockoutComputed<number>;
+
+    // コンストラクタ
+    public constructor() {
+        // 買い物カゴ各行の情報を保持し、それらから合計金額を算出する
+        this.lines = ko.observableArray([new CartLine()]); // デフォルトで1行格納する
+        this.grandTotal = ko.computed({
+            owner: this,
+            read: ():number => {
+                let total:number = 0;
+                this.lines().forEach((v:CartLine,i:number) => { total += v.subtotal() });
+                return total;
+            }
+        });
+    }
+ 
+    // アクション
+    
+    public addLine() { 
+        this.lines.push(new CartLine()) 
+    }
+    
+    public removeLine(line:CartLine) { 
+        this.lines.remove(line) 
+    }
+    
+    public save() {
+        let dataToSave = $.map(this.lines(), (line:CartLine) => {
+            return line.product() ? {
+                productName: line.product().name,
+                quantity: line.quantity()
+            } : undefined
+        });
+        alert("次のようにサーバに送信できます: " + JSON.stringify(dataToSave));
+    }
+
+}
+ 
+ko.applyBindings(new Cart());
