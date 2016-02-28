fca7754ebb8d6ce983f16708bacb0331cc881fba
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-02-28 20:40:21 +0900

    TSとりあえず書いたまで。動かすのはまだ。

--- ts/13_twitterclient.ts
+++ ts/13_twitterclient.ts
@@ -8,97 +8,130 @@
 // そのため常に ViewModel の最新の状態に保たれます。
 // また、ViewModel は View にどのようにバインドされるかを意識せずに記述することができます。
 //
-var savedLists = [
+var savedLists:{name?:string; userNames?:string[]}[] = [
     { name: "Celebrities", userNames: ['JohnCleese', 'MCHammer', 'StephenFry', 'algore', 'StevenSanderson']},
     { name: "Microsoft people", userNames: ['BillGates', 'shanselman', 'ScottGu']},
     { name: "Tech pundits", userNames: ['Scobleizer', 'LeoLaporte', 'techcrunch', 'BoingBoing', 'timoreilly', 'codinghorror']}
 ];
  
-var TwitterListModel = function(lists, selectedList) {
-    this.savedLists = ko.observableArray(lists);
-    this.editingList = {
-        name: ko.observable(selectedList),
-        userNames: ko.observableArray()
-    };
-    this.userNameToAdd = ko.observable("");
-    this.currentTweets = ko.observableArray([])
+class TwitterListModel {
+
+    // 広域変数
+    public editingList:{name?:KnockoutObservable<string>;userNames?:KnockoutObservableArray<string>};
+
+    // Knockoutバインド系
+    public savedLists:KnockoutObservableArray<{name?:string; userNames?:string[]}>;
+    public userNameToAdd:KnockoutObservable<string>;
+    public currentTweets:KnockoutObservableArray<any>;
+    public userNameToAddIsValid:KnockoutComputed<boolean>;
+    public hasUnsavedChanges:KnockoutComputed<boolean>;
+    public canAddUserName:KnockoutComputed<boolean>;
+
+    // コンストラクタ
+    public constructor(
+        lists:{name?:string; userNames?:string[]}[]
+        , selectedList:string
+    ) {
+        this.savedLists = ko.observableArray(lists);
+        this.editingList = {
+            name: ko.observable(selectedList),
+            userNames: ko.observableArray([])
+        };
+        this.userNameToAdd = ko.observable("");
+        this.currentTweets = ko.observableArray([])
+        
+        // computed定義系。
+        
+        this.userNameToAddIsValid = ko.computed(() => {
+            return (this.userNameToAdd() == "") || (this.userNameToAdd().match(/^\s*[a-zA-Z0-9_]{1,15}\s*$/) != null);
+        }, this);
+
+        ko.computed(() => {
+            // viewmodel.editingList.name() を監視する。
+            // 変更があれば、savedList (=データ元) から一致するリスト名のユーザ名リストを取得し、
+            // editingList.userNames にコピーする。
+            let savedList = this.findSavedList(this.editingList.name());
+            if (savedList) {
+                let userNamesCopy = savedList.userNames.slice(0);
+                this.editingList.userNames(userNamesCopy);
+            } else {
+                this.editingList.userNames([]);
+            }
+        }, this);
+        
+        this.hasUnsavedChanges = ko.computed(() => {
+            if (!this.editingList.name()) {
+                return this.editingList.userNames().length > 0;
+            }
+            let savedData:string[] = this.findSavedList(this.editingList.name()).userNames;
+            let editingData:string[] = this.editingList.userNames();
+            return savedData.join("|") != editingData.join("|");
+        }, this);
+        
+            
+        this.userNameToAddIsValid = ko.computed(():boolean => {
+            return (this.userNameToAdd() == "") || (this.userNameToAdd().match(/^\s*[a-zA-Z0-9_]{1,15}\s*$/) != null);
+        }, this);
+    
+        this.canAddUserName = ko.computed(():boolean => {
+            return this.userNameToAddIsValid() && this.userNameToAdd() != "";
+        }, this);
+    
+        // リスト内のユーザのツイートは、(非同期に) editingList.userNames から computed で自動取得する
+        ko.computed(() => {
+            twitterApi.getTweetsForUsers(this.editingList.userNames(), this.currentTweets);
+        }, this);
+
+    }
  
-    this.findSavedList = function(name) {
-        var lists = this.savedLists();
-        return ko.utils.arrayFirst(lists, function(list) {
-            return list.name === name;
+    // メソッド群
+ 
+    public findSavedList(name:string) {
+        let lists:{name?:string; userNames?:string[]}[] = this.savedLists();
+        return ko.utils.arrayFirst(lists, (list:{name?:string; userNames?:string[]}) => {
+            return list.name == name;
         });
-    };
+    }
  
-    this.addUser = function() {
+    public addUser() {
         if (this.userNameToAdd() && this.userNameToAddIsValid()) {
             this.editingList.userNames.push(this.userNameToAdd());
             this.userNameToAdd("");
         }
-    };
+    }
  
-    this.removeUser = function(userName) {
+    public removeUser(userName:string) {
         this.editingList.userNames.remove(userName)
-    }.bind(this);
+    }
  
-    this.saveChanges = function() {
-        var saveAs = prompt("次の名前で保存: ", this.editingList.name());
+    public saveChanges() {
+        let saveAs:string = prompt("次の名前で保存: ", this.editingList.name());
         if (saveAs) {
-            var dataToSave = this.editingList.userNames().slice(0);
-            var existingSavedList = this.findSavedList(saveAs);
-            if (existingSavedList) existingSavedList.userNames = dataToSave; // 既存のリストは上書きする。
-            else this.savedLists.push({
-                name: saveAs,
-                userNames: dataToSave
-            }); // 新しいリストを追加
+            
+            let dataToSave:string[] = this.editingList.userNames().slice(0);
+            let existingSavedList:{name?:string; userNames?:string[]} = this.findSavedList(saveAs);
+            
+            if (existingSavedList) {
+                existingSavedList.userNames = dataToSave; // 既存のリストは上書きする。
+            } else {
+                this.savedLists.push({
+                    name: saveAs,
+                    userNames: dataToSave
+                }); // 新しいリストを追加
+            }
             this.editingList.name(saveAs);
         }
-    };
+    }
  
-    this.deleteList = function() {
-        var nameToDelete = this.editingList.name();
-        var savedListsExceptOneToDelete = $.grep(this.savedLists(), function(list) {
+    public deleteList() {
+        let nameToDelete:string = this.editingList.name();
+        let savedListsExceptOneToDelete:{name?:string; userNames?:string[]}[] = $.grep(this.savedLists(), (list:{name?:string; userNames?:string[]}):boolean => {
             return list.name != nameToDelete
         });
         this.editingList.name(savedListsExceptOneToDelete.length == 0 ? null : savedListsExceptOneToDelete[0].name);
         this.savedLists(savedListsExceptOneToDelete);
-    };
- 
-    ko.computed(function() {
-        // viewmodel.editingList.name() を監視する。
-        // 変更があれば、savedList (=データ元) から一致するリスト名のユーザ名リストを取得し、
-        // editingList.userNames にコピーする。
-        var savedList = this.findSavedList(this.editingList.name());
-        if (savedList) {
-            var userNamesCopy = savedList.userNames.slice(0);
-            this.editingList.userNames(userNamesCopy);
-        } else {
-            this.editingList.userNames([]);
-        }
-    }, this);
- 
-    this.hasUnsavedChanges = ko.computed(function() {
-        if (!this.editingList.name()) {
-            return this.editingList.userNames().length > 0;
-        }
-        var savedData = this.findSavedList(this.editingList.name()).userNames;
-        var editingData = this.editingList.userNames();
-        return savedData.join("|") != editingData.join("|");
-    }, this);
- 
-    this.userNameToAddIsValid = ko.computed(function() {
-        return (this.userNameToAdd() == "") || (this.userNameToAdd().match(/^\s*[a-zA-Z0-9_]{1,15}\s*$/) != null);
-    }, this);
- 
-    this.canAddUserName = ko.computed(function() {
-        return this.userNameToAddIsValid() && this.userNameToAdd() != "";
-    }, this);
- 
-    // リスト内のユーザのツイートは、(非同期に) editingList.userNames から computed で自動取得する
-    ko.computed(function() {
-        twitterApi.getTweetsForUsers(this.editingList.userNames(), this.currentTweets);
-    }, this);
-};
+    }
+}
  
 ko.applyBindings(new TwitterListModel(savedLists, "Tech pundits"));
  
