1ea317f76d72dc14ea350598395969d289f37920
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-02-28 18:01:27 +0900

    とりあえず参照だけ増やしたTSファイルを作成。（未移植）

---
+++ ts/11_gtideditor.ts
@@ -0,0 +1,34 @@
+/// <reference path="./tsd/typings/tsd.d.ts" />
+/// <reference path="./tsd/typings/knockout/knockout.d.ts" />
+/// <reference path="./tsd/typings/jquery.validation/jquery.validation.d.ts" />
+
+var GiftModel = function(gifts) {
+    var self = this;
+    self.gifts = ko.observableArray(gifts);
+ 
+    self.addGift = function() {
+        self.gifts.push({
+            name: "",
+            price: ""
+        });
+    };
+ 
+    self.removeGift = function(gift) {
+        self.gifts.remove(gift);
+    };
+ 
+    self.save = function(form) {
+        alert("次のようにサーバに送信できます: " + ko.utils.stringifyJson(self.gifts));
+        // ここで通常のフォーム送信同様に送信する場合、次のように書いてください:
+        // ko.utils.postJson($("form")[0], self.gifts);
+    };
+};
+ 
+var targetModel = new GiftModel([
+    { name: "高帽子", price: "39.95"},
+    { name: "長いクローク", price: "120.00"}
+]);
+ko.applyBindings(targetModel, document.getElementById('demo_1'));
+ 
+// jQuery Validation を起動
+$("form").validate({ submitHandler: targetModel.save });
